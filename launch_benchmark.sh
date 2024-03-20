59e042b85176e1ee6fc80b50ee726a4687aa587c
jun.du@intel.com <jun.du@intel.com>
2024-03-19 22:12:52 -0700

    update PT run with multi instance

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -53,14 +53,16 @@ function generate_core {
             OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
         elif [ "${device}" == "cuda" ];then
             OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
-	    addtion_options+=" --nv_fuser "
+	        addtion_options+=" --nv_fuser "
+        elif [ "${device}" == "xpu" ];then
+            OOB_EXEC_HEADER=" ZE_AFFINITY_MASK=${i} "
         fi
         printf " ${OOB_EXEC_HEADER} \
-	    python classify.py test --arch drn_d_54 --dummy --pretrained \
-	    	--num_iter $num_iter --num_warmup $num_warmup \
-		--batch-size ${batch_size} \
-		--channels_last $channels_last --precision $precision \
-		--jit --device ${device} \
+	        python classify.py test --arch drn_d_54 --dummy --pretrained \
+                --num_iter $num_iter --num_warmup $num_warmup \
+                --batch-size ${batch_size} \
+                --channels_last $channels_last --precision $precision \
+                --jit --device ${device} \
                 ${addtion_options} \
         > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
         if [ "${numa_nodes_use}" == "0" ];then
